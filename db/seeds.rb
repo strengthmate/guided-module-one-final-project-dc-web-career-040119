7a4354cd920acd494a095a0d4661d363e0e0579e
github username <github email address>
2019-04-15 18:19:29 -0400

    seed movie_genre table

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,26 @@ require 'themoviedb-api'
 require 'pry'
 
 Tmdb::Api.key("9a306747f13ec661784ee120bacdd6fc")
-binding.pry
 Tmdb::Genre.movie_list.each do |genre|
   Genre.find_or_create_by(genre_id: genre.id, name: genre.name)
 end
 
 # Tmdb::Genre.movies(28).results
 
-Tmdb::Genre.movie_list.map do |genre|
+movie_list = Tmdb::Genre.movie_list.map do |genre|
   Tmdb::Genre.movies(genre.id).results
-end.flatten.each do |movie|
-  Movie.find_or_create_by(movie_id: movie.id, name: movie.title, description: movie.overview)
+end.flatten
+
+movie_list.each do |movie|
+  Movie.find_or_create_by(
+    movie_id: movie.id,
+    name: movie.title,
+    description: movie.overview,
+  )
+end
+
+movie_list.each do |movie|
+  movie.genre_ids.each do |genre|
+    MovieGenre.find_or_create_by(movie_id: movie.id, genre_id: genre)
+  end
 end
