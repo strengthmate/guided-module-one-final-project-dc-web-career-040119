1ff5454c783fb2958e8e6ab54b7ac04ab7703695
Matthew <mf9646a@alumni.american.edu>
2019-04-15 17:21:47 -0400

    seeded movies database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 require 'themoviedb-api'
+require 'pry'
 
 Tmdb::Api.key("9a306747f13ec661784ee120bacdd6fc")
-
+binding.pry
 Tmdb::Genre.movie_list.each do |genre|
   Genre.find_or_create_by(genre_id: genre.id, name: genre.name)
+end
+
+# Tmdb::Genre.movies(28).results
+
+Tmdb::Genre.movie_list.map do |genre|
+  Tmdb::Genre.movies(genre.id).results
+end.flatten.each do |movie|
+  Movie.find_or_create_by(movie_id: movie.id, name: movie.title, description: movie.overview)
 end
--- lib/Movie.rb
+++ lib/Movie.rb
@@ -1,3 +1,4 @@
 class Movie < ActiveRecord::Base
-
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
