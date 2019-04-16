ba7234140712b49ff99082c715a1a7ddbc70c139
Matthew <mf9646a@alumni.american.edu>
2019-04-16 17:13:45 -0400

    error handling for too many inputs, and abstractions. Updated text output

--- lib/find_movies.rb
+++ lib/find_movies.rb
@@ -2,10 +2,19 @@ module FindMovies
 
   module ClassMethods
 
+    PREVIOUSLY_ENTERED = {
+      'Genre' => []
+      }
+
+    def get_input
+      input = gets.strip.downcase
+      PREVIOUSLY_ENTERED[self.name] << input
+      input
+    end
 
     def find_movies_by_input
       puts "Enter a film #{self.name.downcase}"
-      input = gets.strip.downcase
+      input = get_input
       if input.downcase != "done"
         self.find_by(name: input).movies
       end
@@ -13,14 +22,23 @@ module FindMovies
 
     def narrow_movie_selection(movie_list:)
       puts "Enter another #{self.name.downcase}"
-      input = gets.strip.downcase
+      input = get_input
       while input != "done"
-        # To do: Check whether movie_list is empty.
-        # If it is, return previous movie list
+        prev_movie_list = movie_list
         movie_list = self.narrow_by_self(movie_list: movie_list, input: input)
+        if movie_list.empty?
+          puts "You have entered too many #{self.name.downcase}s."
+          puts "Here are reccommendations for:"    
+          PREVIOUSLY_ENTERED.each do |key,value| value.pop
+            puts "#{key}: #{value.join(", ")}"
+          end
+          puts prev_movie_list.map {|movie| movie.name}
+        return
+        end
         puts "Enter another #{self.name.downcase}"
-        input = gets.strip.downcase
+        input = get_input
       end
+
       puts movie_list.map {|movie| movie.name}
     end
 
