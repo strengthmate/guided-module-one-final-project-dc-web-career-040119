adfd6e1a1c5205604079bb1da12858369cb93901
github username <github email address>
2019-04-16 18:02:29 -0400

    Abstract methods, add comments for methods in FindMovies, add todo

---
+++ TODO.md
@@ -0,0 +1,10 @@
+#To Do
+* Create, and populate actor table from API
+* Update FindMovies with actor information
+* extract methods into helper methods
+* optimize run file for multiple classes
+* make pretty - optimize UI
+* connect to API
+* Write logic for returning intuitive reccommendations - find percentage match based on search criteria. - sort results based on highest percentage.
+
+* option for exact match or closest match
--- lib/find_movies.rb
+++ lib/find_movies.rb
@@ -2,20 +2,50 @@ module FindMovies
 
   module ClassMethods
 
+    # Store inputs for this session
     PREVIOUSLY_ENTERED = {
       'Genre' => []
-      }
+    }.freeze
 
+    # Get and store input
     def get_input
       input = gets.strip.downcase
       PREVIOUSLY_ENTERED[self.name] << input
       input
     end
 
+    # Print all inputs for this session
+    def output_previously_entered
+      PREVIOUSLY_ENTERED[self.name].pop
+      PREVIOUSLY_ENTERED.each do |key,value|
+        puts "#{key}: #{value.join(", ")}"
+      end
+    end
+
+    # Message for too many inputs
+    def too_many(prev_movie_list)
+      puts "You have entered too many #{self.name.downcase}s."
+      puts "Here are reccommendations for:"
+      self.output_previously_entered
+      puts prev_movie_list.map {|movie| movie.name}
+    end
+
+    #Helper for .narrow_movie_selection
+    def narrow_by_self(movie_list:, input:)
+      movie_list.select do |movie|
+        classes = {
+          'Genre' => movie.genres,
+          # 'Actor' => movie.actors
+        }
+        classes[self.name].include?(self.find_by(name: input))
+      end
+    end
+
+    # Get list of movies from search criteria
     def find_movies_by_input
       puts "Enter a film #{self.name.downcase}"
       input = get_input
-      if input.downcase != "done"
+      unless input.downcase == "done"
         self.find_by(name: input).movies
       end
     end
@@ -26,30 +56,20 @@ module FindMovies
       while input != "done"
         prev_movie_list = movie_list
         movie_list = self.narrow_by_self(movie_list: movie_list, input: input)
+
         if movie_list.empty?
-          puts "You have entered too many #{self.name.downcase}s."
-          puts "Here are reccommendations for:"    
-          PREVIOUSLY_ENTERED.each do |key,value| value.pop
-            puts "#{key}: #{value.join(", ")}"
-          end
-          puts prev_movie_list.map {|movie| movie.name}
-        return
+          too_many(prev_movie_list)
+          return
         end
+
         puts "Enter another #{self.name.downcase}"
         input = get_input
       end
-
+      puts "Here are your reccommendations for:"
+      output_previously_entered
       puts movie_list.map {|movie| movie.name}
     end
 
-    def narrow_by_self(movie_list:, input:)
-      movie_list.select do |movie|
-      classes = {
-      'Genre' => movie.genres,
-      # 'Actor' => movie.actors
-      }
-        classes[self.name].include?(self.find_by(name: input))
-      end
-    end
+
   end
 end
