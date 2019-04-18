65f6e8fc600becef2c37da4b025ec541fb7e512e
github username <github email address>
2019-04-18 18:44:37 -0400

    add end prompt for movie info

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,9 @@
 require_relative '../config/environment'
 require "pry"
+require 'themoviedb-api'
+
+# Authenticate TMDB API
+Tmdb::Api.key("9a306747f13ec661784ee120bacdd6fc")
 
 # Store inputs for this session
 PREVIOUSLY_ENTERED = {
@@ -48,10 +52,73 @@ end
 
 # Output movie recommendations
 def movie_recommendations
-  puts "Here are your recommendations for:".colorize(:yellow)
+  puts "Here are your results for:".colorize(:yellow)
   output_previously_entered
-  puts (SELECTION[:movie_list].map {|movie| movie.name.colorize(:green)})
-  ending_prompt
+  puts (SELECTION[:movie_list].each_with_index.map { |movie, i| "#{i + 1}. #{movie.name}".colorize(:green)})
+  ask_for_movie_info
+  # ending_prompt
+end
+
+def ask_for_movie_info
+  #show description rating reviews date cast trailer link
+  puts ""
+  puts "Select a movie that you would like to learn more about,"
+  puts 'or type "done" to get more recommendations.'
+  puts ""
+
+  input = gets.strip.downcase
+  if input == 'done'
+    ending_prompt
+  else
+    movie_selection = SELECTION[:movie_list].each_with_index.find do |movie, i|
+      input == movie.name.downcase || input.to_i == i + 1
+    end
+    if movie_selection.empty?
+      puts "Sorry, that input was not recognized"
+      ask_for_movie_info
+      return
+    else
+      movie_info(movie_selection[0])
+    end
+  end
+
+end
+
+def movie_info(movie)
+  puts "Here is some information about #{movie.name}"
+  puts ""
+  puts "Website: #{Tmdb::Movie.detail(movie.api_id).homepage}"
+  puts ""
+  puts "Genres: ".colorize(:yellow)
+  Tmdb::Movie.detail(movie.api_id).genres.each { |genre| print "#{genre.name} ,"}
+  puts ""
+  puts "Description:".colorize(:yellow)
+  puts movie.description
+  puts ""
+  puts "Directed by:".colorize(:yellow)
+  Tmdb::Movie.director(movie.api_id).each { |director| print "#{director.name.colorize(:green)}, " } # todo: remove final comma
+  puts ""
+  puts "Released: #{Tmdb::Movie.detail(movie.api_id).release_date[0..3]}"
+  puts ""
+  puts "Cast:".colorize(:yellow)
+  Tmdb::Movie.cast(movie.api_id).each { |actor| print "#{actor.name.colorize(:green)} as #{actor.character}, " }
+  puts ""
+  puts
+  puts "Reviews:"
+  Tmdb::Movie.reviews(movie.api_id).results[0..2].each_with_index {|r, i| puts "Review #{i+1}.".colorize(:yellow) + "#{r.content}\n***\n" }
+  puts ""
+  puts 'Type "done" to return to your results'
+  back_out_movie_info
+end
+
+def back_out_movie_info
+  input = gets.strip.downcase
+  if input == 'done'
+    movie_recommendations
+  else
+    puts 'Input not recognized: Type "done" to return to your results'
+    back_out_movie_info
+  end
 end
 
 def ending_prompt
